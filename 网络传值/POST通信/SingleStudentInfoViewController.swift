116ec432ea2c3ebbc77d81316ff41b30cad8cd6a
USER <user@USERnoMacBook-Pro.local>
2019-02-25 11:43:51 +0900

    纯代码建立两个button

--- 网络传值/POST通信/SingleStudentInfoViewController.swift
+++ 网络传值/POST通信/SingleStudentInfoViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+let ScreenSize = UIScreen.main.bounds
+
 class SingleStudentInfoViewController: UIViewController {
 
     
@@ -15,6 +17,26 @@ class SingleStudentInfoViewController: UIViewController {
     @IBOutlet weak var inputGradeTextField: UITextField!
     @IBOutlet weak var inputBirthTextField: UITextField!
     @IBOutlet weak var inoutPhoneTextField: UITextField!
+    
+    //使用纯代码的方式，建立两个按键，一个叫做修改，一个叫做返回
+    var changeButton:UIButton = {
+        let button = UIButton()
+        button.frame = CGRect(x: ScreenSize.width / 4, y: 500, width: 50, height: 30)
+        button.setTitle("修改", for: UIControl.State.normal)
+        button.backgroundColor = UIColor.lightGray
+        
+        return button
+    }()
+    
+    var backButton:UIButton = {
+        let button = UIButton()
+        button.frame = CGRect(x: (ScreenSize.width / 4) * 3 - 50, y: 500, width: 50, height: 30)
+        button.setTitle("返回", for: UIControl.State.normal)
+        button.backgroundColor = UIColor.lightGray
+        button.addTarget(self, action: #selector(backToMain), for: .touchUpInside)
+        return button
+    }()
+    
     
     var singleStudent:SingleStudentDetail?
     
@@ -26,9 +48,16 @@ class SingleStudentInfoViewController: UIViewController {
         inputGradeTextField.text = singleStudent?.grade
         inputBirthTextField.text = singleStudent?.birthday
         inoutPhoneTextField.text = singleStudent?.phone_number
+        
+        self.view.addSubview(changeButton)
+        self.view.addSubview(backButton)
     }
     
 
+    @objc func backToMain(){
+        dismiss(animated: true, completion: nil)
+    }
+    
 
 
 }
